7501a77e105648b91b53821f0a860cd14f1cfec8
Isa Buitrago <gallegobuitragoisa@gmail.com>
2025-08-27 16:00:08 -0500

    a el archivo de productos.js le agregre los primeros 8 productos que peranecen a la categoria de desayunos

---
+++ src/app/data/productos.js
@@ -0,0 +1,74 @@
+const productos = [
+  {
+    "id": 1,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Dulce Amor",
+    "precio": 85000,
+    "descripcion": "Chocolates, galletas y mini vino.",
+    "tema": "San Valentín",
+    "img": "/src/assets/ancheta1.svg"
+  },
+  {
+    "id": 2,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Premium Café",
+    "precio": 110000,
+    "descripcion": "Café especial, galletas artesanales y taza.",
+    "tema": "Día del Padre",
+    "img": "/src/assets/ancheta2.svg"
+  },
+  {
+    "id": 3,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Tropical",
+    "precio": 98000,
+    "descripcion": "Frutas frescas, chocolates y bebida.",
+    "tema": "Cumpleaños",
+    "img": "/src/assets/ancheta3.svg"
+  },
+  {
+    "id": 4,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Snack Mix",
+    "precio": 72000,
+    "descripcion": "Papitas, maní, gaseosa y dulces.",
+    "tema": "Amigos",
+    "img": "/src/assets/ancheta1.svg"
+  },
+  {
+    "id": 5,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Gourmet",
+    "precio": 145000,
+    "descripcion": "Quesos, salami, crackers y vino.",
+    "tema": "Aniversario",
+    "img": "/src/assets/ancheta2.svg"
+  },
+  {
+    "id": 6,
+    "categoria": "anchetas",
+    "nombre": "Ancheta Chocolovers",
+    "precio": 90000,
+    "descripcion": "Variedad de chocolates premium.",
+    "tema": "Cualquier ocasión",
+    "img": "/src/assets/ancheta3.svg"
+  },
+  {
+    "id": 7,
+    "categoria": "desayunos",
+    "nombre": "Desayuno Clásico",
+    "precio": 65000,
+    "descripcion": "Café, jugo natural, frutas y pan artesanal.",
+    "tema": "Cumpleaños",
+    "img": "/src/assets/desayuno1.svg"
+  },
+  {
+    "id": 8,
+    "categoria": "desayunos",
+    "nombre": "Desayuno Fit",
+    "precio": 69000,
+    "descripcion": "Yogur griego, granola, frutas y té.",
+    "tema": "Fitness",
+    "img": "/src/assets/desayuno2.svg"
+  }
+]
